85a28219f456fe2e3641ae05de68d4ecf0d5f74b
chenhang <1040487567@qq.com>
2023-01-10 23:22:21 +0800

    leetcode 4 array

---
+++ leetcode/climbing-stairs.go
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+70. 爬楼梯
+
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/climbing-stairs
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func climbStairs(n int) int {
+
+}
